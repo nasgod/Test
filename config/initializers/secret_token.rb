b392cc8e54738c9093d71fa005f338ff63b45805
Nasgod <nasgod@NASLINX.(none)>
2012-12-22 09:45:15 +0100

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test::Application.config.secret_token = '3533e15a9ea9fbcc614711c31bc18f2ebeb83f61b8faf659bce1a73cc5b50579c8a37e1188bd4758bb73ec8d7764c14db725f7b21f1f42296ad00c02818782da'
